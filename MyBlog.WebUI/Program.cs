58132fde52a782fc4fb501104b95c8cb91fd6994
Recai <recaiozturk54@gmail.com>
2024-03-04 15:12:44 +0300

    all remote cdns deleted

--- MyBlog.WebUI/Program.cs
+++ MyBlog.WebUI/Program.cs
@@ -11,6 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+
 builder.Services.AddDbContext<ApplicationDbContext>(options => {
     var config = builder.Configuration;
     var connectionString = config.GetConnectionString("database");
